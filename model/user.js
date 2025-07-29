f5da418f66546f0349466ebeea3d721278676c15
surjeetbackend <backendoffice12@gmail.com>
2025-07-29 19:01:39 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -5,13 +5,40 @@ const userSchema = new mongoose.Schema({
     empId: String,
     name: String,
     email: String,
-    phone: String,
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
     password: String,
     gender: String,
     dob: String,
-    hireDate: String,
+  hireDate: { type: Date, required: true },
+
     shift: String,
-    photo: String,   
+    photo: String,
+    
+    paidLeave: {
+    total: {
+      type: Number,
+      default: 14,
+      min: 0
+    },
+    used: {
+      type: Number,
+      default: 0,
+      min: 0
+    }
+  },
+
+  
+
+  
+  role: {
+    type: String,
+    enum: ['employee', 'manager', 'admin'],
+    default: 'employee'
+  }
     
 
 });
