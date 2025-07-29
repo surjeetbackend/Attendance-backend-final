73703f1806356926bbfc8d2fc829039f0258891c
surjeetbackend <backendoffice12@gmail.com>
2025-07-29 19:01:07 +0530

    Update leave.js

--- model/leave.js
+++ model/leave.js
@@ -2,39 +2,22 @@
 const mongoose = require('mongoose');
 
 const LeaveSchema = new mongoose.Schema({
-    employeeId: {
-        type: String,
-        required: true
-    },
-    employeeName: {
-        type: String,
-        required: true
-    },
-    startDate: {
-        type: Date,
-        required: true
-    },
-    endDate: {
-        type: Date,
-        required: true
-    },
-    reason: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'approved', 'rejected'],
-        default: 'pending'
-    },
-    appliedAt: {
-        type: Date,
-        default: Date.now
-    },
-    approveBy:{
-        type:String,
-        required:true
-    }
+  employeeId: { type: String, required: true },
+  employeeName: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  reason: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending'
+  },
+  appliedAt: { type: Date, default: Date.now },
+  approveBy: { type: String, default: '' },
+  leaveType: { type: String, enum: ['paid', 'unpaid', 'partially paid'], default: 'unpaid' },
+  paidDays: { type: Number, default: 0 },
+  unpaidDays: { type: Number, default: 0 }
 });
+
 
 module.exports = mongoose.model('Leave', LeaveSchema);
