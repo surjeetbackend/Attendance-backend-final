a36f3dfb9dbf6d85eed64bfb76025f0ccf81f818
surjeetbackend <backendoffice12@gmail.com>
2026-02-23 13:07:35 +0530

    Update auth.js

--- route/auth.js
+++ route/auth.js
@@ -3,8 +3,63 @@ const router = express.Router();
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
 
+const { protect, authorize } = require("../middleware/auth");
 
-router.post('/register-form', async (req, res) => {
+router.post(
+  "/admin/create-user",
+  // protect,
+  // authorize("admin"),
+  async (req, res) => {
+    try {
+      const { name, email, password, phone, hireDate, role } = req.body;
+
+      if (!name || !email || !password || !phone || !hireDate || !role) {
+        return res.status(400).json({
+          error: "All fields required",
+        });
+      }
+
+      const allowedRoles = ["employee", "manager", "hr_admin", "admin"];
+
+      if (!allowedRoles.includes(role)) {
+        return res.status(400).json({
+          error: "Invalid role",
+        });
+      }
+
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(409).json({ error: "User already exists" });
+      }
+
+      const empId =
+        name.substring(0, 3).toUpperCase() +
+        Math.floor(100 + Math.random() * 900);
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = new User({
+        empId,
+        name,
+        email,
+        phone,
+        hireDate: new Date(hireDate),
+        password: hashedPassword,
+        role,
+      });
+
+      await newUser.save();
+
+      res.json({
+        message: `${role} created successfully`,
+        empId,
+      });
+    } catch (err) {
+      res.status(500).json({ error: "Error creating user" });
+    }
+  }
+);
+router.post('/register-form',protect,authorize('hr_admin'), async (req, res) => {
   try {
     const { name, email, password, phone, photo, gender, hireDate } = req.body;
 
@@ -68,7 +123,7 @@ router.post('/login', async(req, res) => {
             return res.status(401).json({ error: 'Invalid password' });
         }
 
-        res.json({ message: 'Login successful', name: user.name, empId: user.empId });
+        res.json({ message: 'Login successful', name: user.name, empId: user.empId,role:user.role });
 
     } catch (err) {
         console.error('Login error:', err);
