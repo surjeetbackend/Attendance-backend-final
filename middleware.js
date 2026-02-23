bd84dabe1351bce1454a5dfac3dc9954bf5adc42
surjeetbackend <backendoffice12@gmail.com>
2026-02-23 13:36:11 +0530

    Fix import path for User model in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../model/user");
+const User = require("./model/user");
 
 /* =====================================================
    🔐 AUTH MIDDLEWARE
