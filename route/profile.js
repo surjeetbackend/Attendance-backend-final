77db23885751695cf0fa30c6ed34832286dd307d
surjeetbackend <backendoffice12@gmail.com>
2026-04-15 17:46:29 +0530

    Update profile.js

--- route/profile.js
+++ route/profile.js
@@ -1,1 +1,54 @@
 
+const express=require('express')
+const router = express.Router();
+const User=require('../model/user');
+// const { protect, authorize } = require("../middleware/auth");
+// READ by empId
+router.get('/emp/:empId', async (req, res) => {
+  try {
+    const user = await User.findOne({ empId: req.params.empId });
+    
+    if (!user) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.put('/emp/:empId', protect,authorize('employee'),async (req, res) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { empId: req.params.empId },
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete('/emp/:empId',protect,authorize('employee'), async (req, res) => {
+  try {
+    const deletedUser = await User.findOneAndDelete({ empId: req.params.empId });
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+
+    res.json({ message: "Employee deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports=router;
